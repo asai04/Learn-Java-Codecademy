335dc9d3c1376e64e91ce9c6d88efc840e6fce12
asai04 <73530846+asai04@users.noreply.github.com>
2021-10-12 12:48:19 +0530

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree{
+    public static void main(String[] args){
+      // We are going to introduce ourself and print a tree to the screen
+      System.out.println("Hey there, I'm Ashwin and I'm learning to code in Java");
+      System.out.println("Ready to get my hands dirty");
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");   
+    }
+  
+  }
